8fbd0a5344571ab768a9d41cd8724d6515a29600
MalakoN17 <bennynagato17@gmail.com>
2022-10-24 09:05:52 +0300

    updating

---
+++ portfolio-project/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ portfolio-project/pages/_app.js
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import Navbar from "../components/Navbar/Navbar";
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
