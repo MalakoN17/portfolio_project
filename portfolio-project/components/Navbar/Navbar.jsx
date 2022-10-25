9521fe0bfb9d24ae2de74a9163d5e6e010ca27df
MalakoN17 <bennynagato17@gmail.com>
2022-10-25 03:35:31 +0300

    create a main components and adding style scss

--- portfolio-project/components/Navbar/Navbar.jsx
+++ portfolio-project/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Link from "next/link";
 import Dialog from "@mui/material/Dialog";
 import IconButton from "@mui/material/IconButton";
@@ -6,11 +6,11 @@ import MenuIcon from "@mui/icons-material/Menu";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import Slide from "@mui/material/Slide";
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
 
-export default function Navbar() {
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up"   ref={ref} {...props} />;
+});
+function Navbar() {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -22,28 +22,30 @@ export default function Navbar() {
   };
 
   return (
-    <div className="navbar">
-      <MenuIcon variant="outlined" onClick={handleClickOpen} />
-      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition} className="up-menu">
-        <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-          <ArrowDownwardIcon />
-        </IconButton>
-        
-        <div className="menu-container mt-3 flex flex-col items-center gap-3">
-          <Link href="/" className="hover:bg-gray-500">
-            <h1 className="hover:bg-gray-500">Home</h1>
-          </Link>
-          <Link href="/">
-            <h1>About</h1>
-          </Link>
-          <Link href="/">
-            <h1>Skills</h1>
-          </Link>
-          <Link href="/">
-            <h1>Project</h1>
-          </Link>
-        </div>
-      </Dialog>
-    </div>
+<div className="navbar">
+<MenuIcon variant="outlined" onClick={handleClickOpen} />
+<Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition} className="up-menu">
+  <IconButton className="close-btn hover:bg-gray-500 rounded-none" edge="start" color="inherit" onClick={handleClose} aria-label="close">
+    <ArrowDownwardIcon />
+  </IconButton>
+  
+  <div className="menu-container mt-8 flex flex-col items-center gap-3">
+    <Link href="/" className="hover:bg-gray-500">
+      <h1>Home</h1>
+    </Link>
+    <Link href="/">
+      <h1>About</h1>
+    </Link>
+    <Link href="/">
+      <h1>Skills</h1>
+    </Link>
+    <Link href="/">
+      <h1>Project</h1>
+    </Link>
+  </div>
+</Dialog>
+</div>
   );
 }
+
+export default Navbar
---
+++ portfolio-project/components/main/Main.jsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import React, {useState} from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+function Main() {
+    const [scroll, setScroll] = useState(false)
+    const opacityImage = ()=>{
+        if(window.scrollY >= 5){
+            $(".image-container").scss("opacity", 1 - window.scrollY - 250)
+            setScroll(true)
+        }else{
+            setScroll(false)
+        }
+window.addEventListener('scroll', opacityImage)
+}
+  return (
+    <div className='main-container h-screen'>
+        <div className='inner-container max-w-[1240px]'>
+            <div>
+                <h1 className='py-4 text-gray-600'>
+                    Hi, i'm <span className='text-[#339923]'>Malako Nagato</span>
+                </h1>
+                <h1 className='py-2 text-gray-600'>
+                    A Full-Stack Developer 
+                </h1>
+                <p className='py-4 max-w-[70%] m-auto font-bold'>
+                    Lorem ipsum dolor, sit amet consectetur 
+                    adipisicing elit. Ad, atque nihil! Maiores 
+                    accusantium cum veniam odio dolores vel 
+                    quidem quod quo officiis ullam, aliquid 
+                    quisquam fugiat beatae? Officiis, optio 
+                    aspernatur?
+                </p>
+                <div className='icons-container max-w-[330px] py-4'>
+                    <div className='icon-container rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <LinkedInIcon />
+                    </div>
+                    <div className='icon-container rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <GitHubIcon />
+                    </div>
+                    <div className='icon-container rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <MailOutlineIcon />
+                    </div>
+                    <div className='icon-container rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <ContactPageIcon />
+                    </div>
+                </div>
+            </div>
+            <div className={scroll ? 'image-container scroll' : 'image-container'}>
+                <Image src='/../public/assets/images/me-removebg-preview.png' width="500px" height="550px"/>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Main
